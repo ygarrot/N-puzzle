28f11096adc87ebe6504da5ca1d04e4670b97caa
ygarrot <garrot.yoan1@gmail.com>
2020-04-18 10:02:38 +0200

    Makefile and queue

--- source/main.py
+++ source/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+from priority_queue import PriorityQueue
+import fileinput
 
 def main():
-    print("hello world")
+
+    print(fileinput.input().readline())
+    for line in fileinput.input():
+        print(type(line))
+        if line:
+            print("hello world")
+            
+        elem_per_line = len(line.split(" "))
+        print(elem_per_line)
+        print(line)
 
 if __name__ == '__main__':
     main()
