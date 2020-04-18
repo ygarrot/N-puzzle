366bde3728e672ed0f3c5cf1ca3c94178931e327
gbrochar <gbrochar@student.42.fr>
2020-04-18 10:07:07 +0200

    test file + start parser

--- source/main.py
+++ source/main.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+def parse():
+	with open("../test.txt") as f:
+		content = f.read().splitlines()
+	for line in content:
+		line = line.split()
+		for i, word in enumerate(line):
+			if "#" in word:
+				del line[i + 1:]
+				del word[word.index('#'):]
+		print(line)
+	print(content)
+
 def main():
-    print("hello world")
+	parse()
 
 if __name__ == '__main__':
     main()
